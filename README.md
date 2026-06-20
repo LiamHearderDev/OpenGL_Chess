e69f3d1c152e51e3d94512027307665edcd74788
Lemon <LiamHearderDev@gmail.com>
2026-06-20 16:14:15 +0800

    Added `glew`, `glfw`, and `glm`
    Added necessary libraries and CMakeList files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # OpenGL_Chess
 A project for learning to create a 3D playable chess game using OpenGL.
+
+## Modules Used
+OpenGL
+GLFW3
---
+++ src/Window/WindowManager.h
@@ -0,0 +1,12 @@
+#ifndef WINDOW_MANAGER_H
+#define WINDOW_MANAGER_H
+/// A class used to managed open windows.
+
+#define GLFW_INCLUDE_NONE
+
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+
+
+#endif //WINDOW_MANAGER_H
