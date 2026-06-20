50433af9e87d72867ac836a28249f2da98b4c21d
Lemon <LiamHearderDev@gmail.com>
2026-06-20 19:27:14 +0800

    WIP shaders

--- README.md
+++ README.md
@@ -2,5 +2,15 @@
 A project for learning to create a 3D playable chess game using OpenGL.
 
 ## Modules Used
-OpenGL
-GLFW3
+- OpenGL
+- GLFW3
+- GLEW
+- GLAD
+- GLM
+
+## Resources Used
+- https://learnopengl.com/Getting-started
+- https://www.glfw.org/docs/latest/
+- https://github.com/lszl84/glfw_opengl_cmake_template
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,26 @@
 #include <array>
 #include <iostream>
 #include <cstdint>
+#include <fstream>
+
+
+GLuint shaderProgram{};
+
+constexpr auto squareVertices = std::array{
+    -0.5f, 0.5f, 0.0f,  // Top-left
+    0.5f, 0.5f, 0.0f,   // Top-right
+    0.5f, 0.5f, 0.0f,   // Top-right
+    0.5f, -0.5f, 0.0f,  // Bottom-right
+    0.5f, -0.5f, 0.0f,  // Bottom-right
+    -0.5f, -0.5f, 0.0f, // Bottom-left
+    -0.5f, -0.5f, 0.0f, // Bottom-left
+    -0.5f, 0.5f, 0.0f   // Top-left
+};
+
+void error_callback(int error, const char* description);
+void OnFramebufferSizeChanged(GLFWwindow *window, int32_t width, int32_t height);
+void render(GLFWwindow *window);
+GLuint InitShader(const char* vert_shader, const char* frag_shader);
 
 
 int main() {
@@ -43,7 +63,33 @@ int main() {
 		return 1;
 	}
 
-	// TODO: Add OpenGL setup
+
+	// ==== OpenGL setup ==== //
+
+	// Vertex Array Object
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	// Vertex Buffer Object
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(
+		GL_ARRAY_BUFFER, 
+		squareVertices.size() * sizeof(float),
+		squareVertices.data(),
+		GL_STATIC_DRAW
+	);
+
+	// shaders
+	shaderProgram = InitShader("res/shaders/vert.glsl", "res/shaders/frag.glsl");
+	glUseProgram(shaderProgram);
+
+	// Initialise vertex position attribute
+	GLuint vPos = glGetAttribLocation(shaderProgram, "vPosition");
+	glEnableVertex
 
 	
 	// Connect frame buffer size changed callback
@@ -74,6 +120,48 @@ void OnFramebufferSizeChanged(GLFWwindow *window, int32_t width, int32_t height)
 }
 
 void render(GLFWwindow *window){
-	//glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	return;
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(shaderProgram);
+	glDrawArrays(GL_LINES, 0, static_cast<GLsizei>(squareVertices.size()/3));
+	
+	glfwSwapBuffers(window);
 }
+
+std::string loadShaderSource(const char* filepath) {
+    std::ifstream file(filepath);
+    if (!file.is_open()) return "";
+    return std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+}
+
+GLuint InitShader(const char* vert_shader, const char* frag_shader) {
+	// 1. Load source code
+	std::string vCodeStr = loadShaderSource(vert_shader);
+	std::string fCodeStr = loadShaderSource(frag_shader);
+	const char* vCode = vCodeStr.c_str();
+	const char* fCode = fCodeStr.c_str();
+
+	// 2. Create shader objects
+	GLuint vertex = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragment = glCreateShader(GL_FRAGMENT_SHADER);
+
+	// 3. Attach source code and compile
+	glShaderSource(vertex, 1, &vCode, NULL);
+	glCompileShader(vertex);
+	glShaderSource(fragment, 1, &fCode, NULL);
+	glCompileShader(fragment);
+	
+	// 4. Create Program and link shaders
+	GLuint program = glCreateProgram();
+    glAttachShader(program, vertex);
+    glAttachShader(program, fragment);
+    glLinkProgram(program);
+
+	// 5. Clean up (shaders are now linked into the program)
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+
+    return program;
+}
+
